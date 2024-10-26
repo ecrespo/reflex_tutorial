ee92018db4c1081f9f11489250fc95c50d0524b2
Ernesto Crespo <ecrespo@protonmail.com>
2024-10-25 21:48:29 -0400

    second template - dashboard

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -1,39 +1,16 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+"""Welcome to Reflex!."""
+
+# Import all the pages.
+from .pages import *
+from . import styles
 
 import reflex as rx
 
-from rxconfig import config
 
-
-class State(rx.State):
-    """The app state."""
-
-    ...
-
-
-def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
-    )
-
-
-app = rx.App()
-app.add_page(index)
+# Create the app.
+app = rx.App(
+    style=styles.base_style,
+    stylesheets=styles.base_stylesheets,
+    title="Dashboard Template",
+    description="A dashboard template for Reflex.",
+)
