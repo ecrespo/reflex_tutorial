69c5d22ed464e71171862b208a8557db2b52f275
Ernesto Crespo <ecrespo@protonmail.com>
2024-10-27 12:34:50 -0400

    09_reactflow_demo

--- reflex_tutorial/reflex_tutorial.py
+++ reflex_tutorial/reflex_tutorial.py
@@ -1,17 +1,43 @@
 import reflex as rx
 from reflex_qrcode import QRCode
 
+from reflex_tutorial.components.nodes import State
+from reflex_tutorial.components.reactflow import react_flow, background, controls
 
 
 def index() -> rx.Component:
     return rx.flex(
         rx.vstack(
-            rx.heading("Reflex QRCode Demo"),
-            QRCode(
-                title="Esta es una prueba de generación de código QR",
-                value="Prueba",
-                level="H",
-                size=256
+            rx.heading("Reflex ReactFlow Demo"),
+            rx.vstack(
+                react_flow(
+                    background(),
+                    controls(),
+                    nodes_draggable=True,
+                    nodes_connectable=True,
+                    on_connect=lambda e0: State.on_connect(e0),
+                    on_nodes_change=lambda e0: State.on_nodes_change(
+                        e0
+                    ),
+                    nodes=State.nodes,
+                    edges=State.edges,
+                    fit_view=True,
+                ),
+                rx.hstack(
+                    rx.button(
+                        "Clear graph",
+                        on_click=State.clear_graph,
+                        width="100%",
+                    ),
+                    rx.button(
+                        "Add node",
+                        on_click=State.add_random_node,
+                        width="100%",
+                    ),
+                    width="100%",
+                ),
+                height="30em",
+                width="100%",
             )
         ),
     )
